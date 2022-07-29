97891ec941ce0b07b58a48321a8344906cf2923e
maggnad <maggnad@gmail.com>
2022-07-29 19:50:03 +0500

    script active added

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ btn.addEventListener('click', function () {
     btn.classList.toggle("active")
     menu.classList.toggle("active")
 })
+
+const menuLink = document.querySelectorAll(".menu__link")
+for (let i = 0; i < menuLink.length; i++) {
+    const el = menuLink[i];
+    el.addEventListener('click', function () {
+        for (let i = 0; i < menuLink.length; i++) {
+            menuLink[i].classList.remove('active')
+        }
+        this.classList.add('active')
+    })
+}
